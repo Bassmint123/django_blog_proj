1402c3f28841f15ce8bdec4acf8826e334a69117
Bassmint123 <bradb@cimstrat.com>
2019-06-28 11:49:37 -0500

    Added detailed blog views and lists

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import BlogListView
+from .views import BlogListView, BlogDetailView 
 
 urlpatterns = [
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'), # see the details of each blog post
     path('', BlogListView.as_view(), name='home'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView  # added the DetailView for individual blog pages
 from .models import Post
 
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
 
+class BlogDetailView(DetailView):
+	model = Post
+	template_name = 'post_detail.html'
