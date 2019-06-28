a63f7753f302cfb881e716ec6683da23188f4705
Bassmint123 <bradb@cimstrat.com>
2019-06-28 15:25:06 -0500

    Added the ability to delete and test

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import ListView, DetailView  # added the views for individual blog pages
-from django.views.generic.edit import CreateView, UpdateView # using the generic edit for creating a new post
+from django.views.generic.edit import CreateView, UpdateView, DeleteView # using the generic edit for creating a new post
+# We use reverse_lazy as opposed to just reverse so that it won’t execute the URL
+# redirect until our view has finished deleting the blog post.
+from django.urls import reverse_lazy # new
+
 from .models import Post
 
 class BlogListView(ListView):
@@ -22,3 +26,8 @@ class BlogUpdateView(UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ['title', 'body']	
+
+class BlogDeleteView(DeleteView): # new
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
